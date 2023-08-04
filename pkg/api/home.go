238fef437f31e9a5fc6ddbd17e87da8a9f309a92
Fredrik Averpil <3138143+kerumitdg@users.noreply.github.com>
2023-08-04 19:56:47 +0200

    chore: don't use service for catchall/home

--- pkg/api/home.go
+++ pkg/api/home.go
@@ -1,12 +1,8 @@
 package api
 
-import (
-	"net/http"
-
-	"github.com/fredrikaverpil/go-api-std/pkg/services"
-)
+import "net/http"
 
 func (s *Server) DefaultHandler(w http.ResponseWriter, r *http.Request) {
-	message := services.Home()
+	message := "hello world"
 	w.Write([]byte(message))
 }
