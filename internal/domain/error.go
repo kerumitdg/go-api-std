50c9f1d5b317b15ff78846b24c74084b9e106a6d
Fredrik Averpil <3138143+kerumitdg@users.noreply.github.com>
2023-10-08 09:20:35 +0200

    docs: add comments to explain intent

--- internal/domain/error.go
+++ internal/domain/error.go
@@ -1,3 +1,20 @@
+/*
+Domain errors.
+
+The domain error is a custom error type that can be used to return errors from the domain layer and have them
+automatically mapped to a desired API response in the API layer. For example, if the domain layer returns
+a NotFoundError, then the API layer will automatically return a 404 Not Found response for the REST API.
+
+Each domain error carries a code. This code is used by each API's error-to-response mapper. See each API's mapper
+implementation for more details.
+
+WARNING: Anytime a domain error is returned, it is assumed this error does not contain any sensitive data that can
+be considered a security risk if leaked by mistake to the client. All domain errors are assumed to be safe. If you
+are unsure, don't return a domain error. Instead, return a regular error. The intent is that the API mapper should
+always produce an internal server error for regular errors without revealing the details of the underlying error
+to the client. Logging should instead be used to capture the details of the error.
+*/
+
 package domain
 
 import (
@@ -11,9 +28,10 @@ const (
 	ErrNotFound        ErrorCode = iota
 	ErrConflict        ErrorCode = iota
 	ErrInvalidArgument ErrorCode = iota
-	// Add more error codes here
+	// Add more error codes here and then add the mapping in each APIs error-to-response mapper
 )
 
+// Domain error
 type Error struct {
 	Code    ErrorCode
 	Message string
--- internal/rest/error_resp_mapper.go
+++ internal/rest/error_resp_mapper.go
@@ -7,7 +7,20 @@ import (
 	"github.com/fredrikaverpil/go-api-std/internal/domain"
 )
 
+// Map custom domain errors (and regular errors) to REST responses
 func mapErrorToRESTResponse(err error, validHTTPStatuses []int, w http.ResponseWriter) {
+	// To avoid leaking undesired errors, the caller to this function must provide a list of valid HTTP status codes.
+	// If the error code is not in the list, then a generic 500 Internal Server Error is returned.
+	//
+	// The reasoning behind this is that each endpoint usually needs to add documentation on which possible
+	// HTTP status codes it can return. By forcing the caller to provide a list of valid HTTP status codes,
+	// we can ensure that the caller has thought about which HTTP status codes are valid for the endpoint.
+	//
+	// One might argue that it is more important to instead provide a list of valid domain error codes, so to avoid
+	// leaking internal errors. However, this is not always possible. For example, if the endpoint is a POST endpoint,
+	// then it is not possible to know which error codes are valid, since the error codes are dependent on the data
+	// provided in the request body.
+
 	codeToHTTPStatus := map[domain.ErrorCode]int{
 		domain.ErrNotFound:        http.StatusNotFound,
 		domain.ErrConflict:        http.StatusConflict,
